b20e9086e465e1b52000ad7ff1105448c6112f38
TechGeekCode <techgeekcode@mailinator.com>
2023-12-10 14:58:52 +0530

    Update script.php
    check file validation function

--- script.php
+++ script.php
@@ -8,6 +8,17 @@ function main($argc, $argv) {
 
 }
 
+// check file validation
+function validateCommandLineArguments($argc, $argv) {
+   if ($argc != 3 && $argv[0] != "MovieRatingTest.php") {
+        echo "Expected command is: php script.php reviews.json movies.json\n";
+        exit(1);
+    } elseif (@$argv[1] != "reviews.json" && $argv[0] != "MovieRatingTest.php") {
+        echo "Expected order is: php script.php reviews.json movies.json\n";
+        exit(1);
+    }
+}
+
 // to read JSON data from files and store them in variables.
 function getJsonData($fileName, $errorMessage) {
     $jsonData = file_get_contents($fileName);
